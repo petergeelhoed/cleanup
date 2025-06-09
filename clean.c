87b38f9cf50842eb846ccae22a402060e755e584
petergeelhoed <petergeelhoed@gmail.com>
2025-06-09 11:10:42 +0200

    struct

--- clean.c
+++ clean.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void clean(char** p)
+struct PERSON
 {
-    puts(__func__);
+    char* name;
+    int age;
+};
+
+void cleanPerson(struct PERSON** p)
+{
+    printf("%s %p\n", __func__, (*p)->name);
+    free((*p)->name);
     free(*p);
 }
 
 void make()
 {
-    puts(__func__);
-    char* c __attribute__((cleanup(clean))) = malloc(sizeof(char) * 7);
-    strncpy(c, "hello", 6);
-    puts(c);
+    struct PERSON* p __attribute__((cleanup(cleanPerson))) = malloc(sizeof(struct PERSON));
+    p->name = (char*)malloc(sizeof(char) * 7);
+    printf("%s %p\n", __func__, p->name);
+    strncpy(p->name, "hello", 6);
+    puts(p->name);
 }
 
 int main()
